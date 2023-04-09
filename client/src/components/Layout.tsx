6e807f1f2ac497218e937416ba82ded924b77d39
jazzes <j4zzes@gmail.com>
2023-04-09 21:40:00 +0300

    mainpage done

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../csscomponents/layout.scss"
+import "../csscomponents/fakestore.scss"
 import {Link, Outlet} from 'react-router-dom'
 import logo from "../static/photos/logo.svg"
 import basket from "../static/photos/basket.svg"
 
 const Layout = () => {
+
+    const [active, setActive] = useState(false)
+    const burgerActive = active ? "burger-menu burger-activated" : "burger-menu"
+
+
     return (
         <div className="wrapper">
             <div className="nav">
                 <Link to="/" className="logoImage">
-                    <img src={logo}/>
+                    <img alt="" src={logo}/>
                 </Link>
-                <div className="">
-                    <Link to="/basket">
-                        <img src={basket}/>
+                <div className="nav__right_side">
+                    <Link className="basketLogo" to="/basket">
+                        <img alt="" src={basket}/>
                     </Link>
+                    <div className={burgerActive} onClick={ () => {setActive(active => !active)} }>
+                        <span></span>
+                    </div>
                 </div>
             </div>
 
             <div className="main">
+                <div className="background"></div>
                 < Outlet />
             </div>
 
             <div className="footer">
                 <Link to='/contact' className="linkToContact">Contact</Link>
-                <div className="linkToOfficial">Lisichkin © 2023</div>
+                <h1 className="linkToOfficial">Lisichkin © 2023</h1>
             </div>
         </div>
 
